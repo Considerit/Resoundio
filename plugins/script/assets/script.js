f7396f50843d9b854771dfacadef90dd316db886
Travis Kriplean <travis@consider.it>
2024-03-28 13:37:48 -0700

    add to document.body, not shadowroot

--- plugins/script/assets/script.js
+++ plugins/script/assets/script.js
@@ -6,22 +6,23 @@ const hd = window.hyperdiv
 /// Plugin constructor has 3 arguments: key (the html element id), the shadow DOM root, and initial props.
 hd.registerPlugin('Script', function(key, shadow_root, initial_props) {
 
-
-    // const script = document.createElement('script')
-
     const parser = new DOMParser()
     const doc = parser.parseFromString(initial_props.defn, 'text/html')
     const scriptFromString = doc.querySelector('script')
 
     const script = document.createElement('script')
 
+    let attrs = ""
     for (let attr of scriptFromString.attributes) {
       script.setAttribute(attr.name, attr.value);
+      attrs += "-" + attr.name + ":" + attr.value
     }
 
-
-    shadow_root.appendChild(script)
-    // document.body.appendChild(script)
+    let script_key = encodeURI(attrs)
+    if (!document.body.querySelector("[data-script-key=\"" + script_key + "\"]")){
+        script.setAttribute("data-script-key", script_key)
+        document.body.appendChild(script) // note: adding to shadow_root will cause problems
+    }
 
     return function(prop_key, prop_value) {
         console.log('updated property key', prop_key)
